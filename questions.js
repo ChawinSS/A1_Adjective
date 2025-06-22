1b51e61e3a8e68db235fd0f41e6836675b104d2d
ChawinSS <137822600+ChawinSS@users.noreply.github.com>
2025-06-22 12:19:11 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,272 @@
+const quizDataFromFile = [
+  {
+    "question": "böse",
+    "correct": "angry",
+    "choices": [
+      "…",
+      "female",
+      "angry",
+      "sharp"
+    ]
+  },
+  {
+    "question": "falsch",
+    "correct": "wrong / incorrect",
+    "choices": [
+      "good",
+      "wrong / incorrect",
+      "possible",
+      "light"
+    ]
+  },
+  {
+    "question": "glücklich",
+    "correct": "happily",
+    "choices": [
+      "good",
+      "male",
+      "happily",
+      "small"
+    ]
+  },
+  {
+    "question": "groß",
+    "correct": "tall",
+    "choices": [
+      "wrong / incorrect",
+      "large",
+      "tall",
+      "new"
+    ]
+  },
+  {
+    "question": "groß",
+    "correct": "large",
+    "choices": [
+      "good",
+      "large",
+      "angry",
+      "new"
+    ]
+  },
+  {
+    "question": "gut",
+    "correct": "good",
+    "choices": [
+      "good",
+      "punctual",
+      "right / correct",
+      "…"
+    ]
+  },
+  {
+    "question": "gut",
+    "correct": "good",
+    "choices": [
+      "good",
+      "happily",
+      "green",
+      "angry"
+    ]
+  },
+  {
+    "question": "gut",
+    "correct": "good",
+    "choices": [
+      "good",
+      "green",
+      "tall",
+      "small"
+    ]
+  },
+  {
+    "question": "gut",
+    "correct": "…",
+    "choices": [
+      "black",
+      "wrong / incorrect",
+      "…",
+      "light"
+    ]
+  },
+  {
+    "question": "gut",
+    "correct": "…",
+    "choices": [
+      "good",
+      "possible",
+      "…",
+      "light"
+    ]
+  },
+  {
+    "question": "hell",
+    "correct": "light",
+    "choices": [
+      "angry",
+      "red",
+      "…",
+      "light"
+    ]
+  },
+  {
+    "question": "herzlich",
+    "correct": "…",
+    "choices": [
+      "happily",
+      "large",
+      "…",
+      "blue"
+    ]
+  },
+  {
+    "question": "klein",
+    "correct": "small",
+    "choices": [
+      "good",
+      "black",
+      "female",
+      "small"
+    ]
+  },
+  {
+    "question": "männlich",
+    "correct": "male",
+    "choices": [
+      "male",
+      "happily",
+      "green",
+      "…"
+    ]
+  },
+  {
+    "question": "möglich",
+    "correct": "possible",
+    "choices": [
+      "good",
+      "possible",
+      "angry",
+      "correct"
+    ]
+  },
+  {
+    "question": "neu",
+    "correct": "new",
+    "choices": [
+      "good",
+      "black",
+      "wrong / incorrect",
+      "new"
+    ]
+  },
+  {
+    "question": "neu",
+    "correct": "new",
+    "choices": [
+      "wrong / incorrect",
+      "blue",
+      "large",
+      "new"
+    ]
+  },
+  {
+    "question": "pünktlich",
+    "correct": "sharp",
+    "choices": [
+      "good",
+      "new",
+      "happily",
+      "sharp"
+    ]
+  },
+  {
+    "question": "pünktlich",
+    "correct": "punctual",
+    "choices": [
+      "good",
+      "punctual",
+      "large",
+      "red"
+    ]
+  },
+  {
+    "question": "richtig",
+    "correct": "correct",
+    "choices": [
+      "punctual",
+      "sharp",
+      "white",
+      "correct"
+    ]
+  },
+  {
+    "question": "richtig",
+    "correct": "right / correct",
+    "choices": [
+      "good",
+      "black",
+      "right / correct",
+      "…"
+    ]
+  },
+  {
+    "question": "weiblich",
+    "correct": "female",
+    "choices": [
+      "happily",
+      "green",
+      "female",
+      "new"
+    ]
+  },
+  {
+    "question": "schwarz",
+    "correct": "black",
+    "choices": [
+      "black",
+      "right / correct",
+      "wrong / incorrect",
+      "new"
+    ]
+  },
+  {
+    "question": "blau",
+    "correct": "blue",
+    "choices": [
+      "new",
+      "possible",
+      "female",
+      "blue"
+    ]
+  },
+  {
+    "question": "grün",
+    "correct": "green",
+    "choices": [
+      "good",
+      "white",
+      "green",
+      "sharp"
+    ]
+  },
+  {
+    "question": "weiß",
+    "correct": "white",
+    "choices": [
+      "possible",
+      "white",
+      "red",
+      "…"
+    ]
+  },
+  {
+    "question": "rot",
+    "correct": "red",
+    "choices": [
+      "new",
+      "punctual",
+      "red",
+      "correct"
+    ]
+  }
+];
